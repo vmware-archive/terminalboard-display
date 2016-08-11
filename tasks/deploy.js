99e4e616cec2a57e5a3c7fb57801dc83cb625f13
Chris Hendrix <chendrix@pivotal.io>
2016-08-11 13:55:24 -0700

    Copy in vendored js dependencies
    [#128177719]
    
    Signed-off-by: Andrew Crump <acrump@pivotal.io>

--- tasks/deploy.js
+++ tasks/deploy.js
@@ -10,10 +10,14 @@ gulp.task('copy-staticfile', () => {
   return gulp.src('Staticfile').pipe(gulp.dest('public'));
 });
 
+gulp.task('copy-vendor', () => {
+  return gulp.src('vendor/*.js').pipe(gulp.dest('public'));
+});
+
 gulp.task('deploy', (done) => {
   const {NODE_ENV: env} = process.env;
   process.env.NODE_ENV = 'production';
-  runSequence('assets', 'copy-staticfile', 'push', () => {
+  runSequence('assets', 'copy-staticfile', 'copy-vendor', 'push', () => {
     process.env.NODE_ENV = env;
     done();
   });
