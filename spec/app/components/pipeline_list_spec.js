4e03b5dd93f12b0c44db7e602952e757fcbf2da0
Chris Hendrix <chendrix@pivotal.io>
2016-08-11 17:00:25 -0700

    Backfill test for showing only unsuccessful builds
    [#128177719]
    
    Signed-off-by: Andrew Crump <acrump@pivotal.io>

---
+++ spec/app/components/pipeline_list_spec.js
@@ -0,0 +1,40 @@
+require('../spec_helper');
+const ReactTestUtils = require('react-addons-test-utils');
+
+describe('PipelineList', () => {
+  var renderer;
+  beforeEach(() => {
+    const PipelineList = require('../../../app/components/pipeline_list');
+
+    renderer = ReactTestUtils.createRenderer();
+    renderer.render(<PipelineList pipelines={
+      [{
+        pipelineName: 'foo',
+        pipelineStatus: 'success',
+        currentlyRunning: false,
+        url: 'https://example.com/foo'
+      },
+      {
+        pipelineName: 'bar',
+        pipelineStatus: 'failure',
+        currentlyRunning: true,
+        url: 'https://example.com/bar'
+      },
+      {
+          pipelineName: 'baz',
+        pipelineStatus: 'notARealStatus',
+        currentlyRunning: true,
+        url: 'https://example.com/baz'
+      }]
+    } />, root);
+  });
+
+  it('renders anything other than successful builds', () => {
+    var result = renderer.getRenderOutput();
+
+    var children = result.props.children;
+    expect(children).toHaveLength(2);
+    expect(children[0].props.pipelineName).toEqual("bar");
+    expect(children[1].props.pipelineName).toEqual("baz");
+  })
+});
